fde234852b370fd3e53aa5b732e35ee09d966c3e
Alejandro García Serrano <agscm2@gmail.com>
2025-03-06 17:28:22 +0100

    Subida

---
+++ ejercicios/Modulo3/Ejercicios_clase/ejercicios3.2.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * fórmula
+ * fib(1) = 1;
+ * fib(2) = 1;
+ * firb(n) = fin(n-2) + fib(n-1)
+ * serie 1, 1, 2, 3, 5, 8, 13, 21, ... 
+ */
+
+function fibonacci(num) {
+    if (num === 1 || num === 2) return 1;
+
+    return (fibonacci(num-2)+fibonacci(num-2));
+}
+
+fibonacci(6);
